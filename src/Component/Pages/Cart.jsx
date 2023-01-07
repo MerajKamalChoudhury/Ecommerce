a941ce85161ad0e2a9535b7095b0c6de01f74d3a
Jahir <jahirmj2001@gmail.com>
2023-01-07 23:32:42 +0530

    Create New Pages

---
+++ src/Component/Pages/Cart.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+
+
+const Title = styled.h1`
+    position: absolute;
+    top: 50%;
+    left: 45%;
+`;
+
+const Cart = () => {
+    return (
+        <>
+            <Title>This is Cart Page</Title>
+        </>
+    )
+}
+
+export default Cart
